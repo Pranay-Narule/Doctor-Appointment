ff17e0e1fb0bd7f5285917a92f1721221f8a8fd9
Pranay Narule <pranaynarule564@gmail.com>
2021-10-02 17:40:09 +0530

    Update db_connection.php
    New database entries

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,10 @@
 <?php
 function OpenCon()
  {
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "";
- $db = "doc_app_db";
+ $dbhost = "	sql304.epizy.com";
+ $dbuser = "epiz_29918512";
+ $dbpass = "Pranay1996@@";
+ $db = "epiz_29918512_doc_app_db";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
